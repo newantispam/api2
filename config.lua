a84f8e12630131b4239cf372a97ef687dff81142
newantispam <newantispam@mihanmail.ir>
2016-07-08 10:28:32 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,17 @@
 return {
-	bot_api_key = '',
+	bot_api_key = '251131038:AAG_GzTtcHkXDj0jCmr7s9UzAmpci9J6Io4',
 	version = '3.1', -- /aupdate for v3.1
 	testing_mode = false,
 	admin = {
-		owner = 23646077,
+		owner = 166111645,
 		admins = {
-			[141956218] = true,
-			[23646077] = true
+			[166111645] = true,
+			[163769705] = true
 		}
 	},
 	log_chat = -1001057801239,
 	channel = '@groupbutler_ch', --channel username with the '@'
-	help_group = 'https://telegram.me/joinchat/CHYUej9jFxFN12MQqrLnUg', --group link, not username!
+	help_group = '@savior_team', --group link, not username!
 	languages = 'languages.lua',
 	plugins = {
 		'onmessage.lua', --THIS HAVE TO BE THE FIRST: IF AN USER IS SPAMMING/IS BLOCKED, THE BOT WON'T GO THROUGH PLUGINS
